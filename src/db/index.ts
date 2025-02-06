34b73157b169ef93724c7d2104b58a338197d9e4
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-05 22:25:54 -0800

    update export

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,4 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "@/env/server";
 
-const db = drizzle(env.PG_DATABASE_URL);
-
-export default db;
+export const db = drizzle(env.PG_DATABASE_URL);
