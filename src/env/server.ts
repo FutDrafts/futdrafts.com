540307e48668595eac39486cb4510d86fb2861a7
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-05 23:06:00 -0800

    add auth (#8)
    * deps: install drizzle
    
    * configure drizzle
    
    * init db and schema
    
    * update export
    
    * add github oauth
    
    * deps: install better-auth
    
    * add api route
    
    * deps: install auth deps
    
    * add auth schemas
    
    * add auth middleware
    
    * configure better auth client

--- src/env/server.ts
+++ src/env/server.ts
@@ -4,6 +4,10 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     PG_DATABASE_URL: z.string(),
+    BETTER_AUTH_SECRET: z.string(),
+    BETTER_AUTH_URL: z.string(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
   },
   experimental__runtimeEnv: process.env,
 });
---
+++ src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { betterAuth } from "better-auth";
+import { admin, jwt } from "better-auth/plugins";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db";
+import { env } from "@/env/server";
+
+export const auth = betterAuth({
+  appName: "FutDrafts",
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: true,
+    autoSignIn: true,
+    maxPasswordLength: 32,
+  },
+  socialProviders: {
+    github: {
+      enabled: true,
+      clientId: env.GITHUB_CLIENT_ID as string,
+      clientSecret: env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [admin(), jwt()],
+});
