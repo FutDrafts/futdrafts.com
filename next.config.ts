c708f1f62250dc240de68776568a0dad9df76af9
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-05 22:03:34 -0800

    configure t3-env

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,10 @@
 import type { NextConfig } from "next";
+import { createJiti } from "jiti";
+import { fileURLToPath } from "node:url";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+jiti.esmResolve("./src/env/client.ts");
+jiti.esmResolve("./src/env/server.ts");
 
 const nextConfig: NextConfig = {
   /* config options here */
