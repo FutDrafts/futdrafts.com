bf8c5c55698908e5a2d98c9d1f9ebd1d88fe18fa
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-07 03:19:41 -0800

    fix: missing schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,5 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { env } from "@/env/server";
+import * as schema from "@/db/schema";
 
-export const db = drizzle(env.PG_DATABASE_URL);
+export const db = drizzle(env.PG_DATABASE_URL, { schema });
