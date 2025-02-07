3817f7d0c5e1e3cd4428a34719602144abec90ee
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-07 03:26:58 -0800

    add shadcn components (#19)
    * deps: add component packages
    
    * cn util
    
    * add components
    
    * hooks

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,3 +17,12 @@ export function urlBase64ToUint8Array(base64String: string) {
   }
   return outputArray;
 }
+
+export async function convertImageToBase64(file: File): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result as string);
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+}
