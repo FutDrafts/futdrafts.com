1c360adc0e3bf332b630525dc6258236ce443e72
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-06 21:51:36 -0800

    add mdx support (#10)
    * deps: install drizzle
    
    * configure drizzle
    
    * init db and schema
    
    * update export
    
    * add github oauth
    
    * deps: install better-auth
    
    * add api route
    
    * deps: install auth deps
    
    * add auth schemas
    
    * add auth middleware
    
    * configure better auth client
    
    * deps: install mdx deps
    
    * configure mdx support
    
    * add temp pages

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import createMdx from "@next/mdx";
 import { createJiti } from "jiti";
 import { fileURLToPath } from "node:url";
 
@@ -8,6 +9,9 @@ jiti.esmResolve("./src/env/server.ts");
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ["ts", "tsx", "js", "jsx", "mdx"],
 };
 
-export default nextConfig;
+const withMDX = createMdx({});
+
+export default withMDX(nextConfig);
