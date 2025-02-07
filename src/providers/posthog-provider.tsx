44b02604af25beb7bf8ece2de49ba5095f52fca5
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-07 01:35:59 -0800

    update posthog config

--- src/providers/posthog-provider.tsx
+++ src/providers/posthog-provider.tsx
@@ -3,16 +3,17 @@
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 import { env } from "@/env/client";
-
-if (typeof window !== "undefined") {
-  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: env.NEXT_PUBLIC_POSTHOG_KEY!,
-    person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-  });
-}
+import { useEffect } from "react";
 
 export function ClientPostHogProvider(
   { children }: { children: React.ReactNode },
 ) {
+  useEffect(() => {
+    posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+      person_profiles: 'always',
+    })
+  }, [])
+
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
