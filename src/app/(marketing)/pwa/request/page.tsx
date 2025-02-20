b21a5ba39e3d3e93a5997444b9328c87f14109c0
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-20 02:26:45 -0800

    fix cors issues to connect next to fastapi (#29)

---
+++ src/app/(marketing)/pwa/request/page.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { useToken } from '@/hooks/use-token'
+import { Loader2 } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
+
+export default function TestRequestPage() {
+    const { token, isLoading, isError } = useToken()
+    const [data, setData] = useState<unknown>()
+
+    if (isLoading)
+        <div className="flex h-screen w-screen flex-col items-center">
+            <Loader2 className="animate-spin" />
+        </div>
+    if (isError) toast.error(isError)
+
+    useEffect(() => {
+        function getData() {
+            const datas = fetch('http://localhost:80/private', {
+                method: 'GET',
+                credentials: 'include',
+                mode: 'cors',
+                referrer: 'http://localhost:3000/pwa/request',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }).then((res) => res.json())
+
+            setData(datas)
+        }
+
+        getData()
+    }, [token])
+
+    return (
+        <div className="container flex h-screen flex-col items-center justify-center gap-4">
+            <p className="text-wrap text-xs">{data ? JSON.stringify(data) : 'Request probably failed'}</p>
+        </div>
+    )
+}
