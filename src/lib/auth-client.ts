c0d3ad4736cf4e657e25a45f98b116f1a7f668a5
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-21 18:26:47 -0800

    auth CORS fixes (#32)
    * update auth config
    
    * fix: lint
    
    * remove export

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -5,4 +5,10 @@ import { adminClient } from 'better-auth/client/plugins'
 export const authClient = createAuthClient({
     baseURL: env.NEXT_PUBLIC_APP_URL,
     plugins: [adminClient()],
+    fetchOptions: {
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    },
 })
