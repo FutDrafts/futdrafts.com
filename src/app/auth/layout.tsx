ec0a1c6a95a680b477a427e5426bfe65ddd3202b
Kourosh Alasti <coding@kouroshalasti.com>
2025-02-25 17:29:54 -0800

    add redirects on existing session

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,17 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
+
+export default async function AuthLayout({ children }: { children: React.ReactNode }) {
+    const [session] = await Promise.all([
+        await auth.api.getSession({
+            headers: await headers()
+        })
+    ]).catch(() => redirect("/auth/sign-ing"))
+
+    if(session) {
+        redirect("/dashboard")
+    }
+    
     return <div className="flex h-screen flex-col items-center justify-center">{children}</div>
 }
