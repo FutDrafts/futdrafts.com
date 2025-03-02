8f4f51469609a538935b4a1600cb57e88014ec81
Kourosh Alasti <coding@kouroshalasti.com>
2025-03-02 05:31:13 -0800

    fix: update route handler

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,6 +1,4 @@
 import { auth } from '@/lib/auth'
 import { toNextJsHandler } from 'better-auth/next-js'
 
-const handler = toNextJsHandler(auth)
-
-export const { GET, POST } = handler
+export const { GET, POST } = toNextJsHandler(auth)
