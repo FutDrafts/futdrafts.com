476e536d3943677f147cc6179cb5f57bb428203b
Kourosh Alasti <coding@kouroshalasti.com>
2025-03-03 06:58:30 -0800

    fix: disable react scan in production

--- src/providers/posthog-provider.tsx
+++ src/providers/posthog-provider.tsx
@@ -8,16 +8,20 @@ import { useEffect } from 'react'
 
 export function ClientPostHogProvider({ children }: { children: React.ReactNode }) {
     useEffect(() => {
-        scan({
-            enabled: process.env.NODE_ENV !== 'production',
-            log: true,
-        })
+        
 
         if (!window.location.host.includes('127.0.0.1') && !window.location.host.includes('localhost')) {
             posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
                 api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
                 person_profiles: 'always',
             })
+
+            if (!posthog.isFeatureEnabled('react-scan') ) {
+                scan({
+                    enabled: process.env.NODE_ENV !== 'production',
+                    log: true,
+                })
+            }
         }
     }, [])
 
