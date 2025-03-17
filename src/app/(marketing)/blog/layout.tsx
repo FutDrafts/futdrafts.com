53dc44948c59a728f261cb78abf23d7b9a1b5544
Kourosh Alasti <coding@kouroshalasti.com>
2025-03-16 22:34:18 -0700

    build: format and lint

--- src/app/(marketing)/blog/layout.tsx
+++ src/app/(marketing)/blog/layout.tsx
@@ -1,5 +1,3 @@
 export default async function BlogLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <main className="container mx-auto px-4 py-8">{children}</main>
-    )
+    return <main className="container mx-auto px-4 py-8">{children}</main>
 }
